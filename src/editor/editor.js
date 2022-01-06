8b0c7457f6d4d1628183ab40966b52ba9b0e1552
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2022-01-06 17:52:28 +0530

    img

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -107,12 +107,18 @@ const ReactEditor = () => {
               }
             },
             {
-              type: "graph",
+              type: "image",
               data: {
-                text:
-                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
+                file: {
+                  url:
+                    "../download.png"
+                },
               }
             },
+            {
+              type: "graph",
+            },
+
           ],
           version: "2.12.4"
         }}
