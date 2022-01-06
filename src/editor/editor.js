60a8195edb67b40ef22ece2cbed6106611618ab1
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2022-01-06 17:35:29 +0530

    graph added

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -106,6 +106,13 @@ const ReactEditor = () => {
                   "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
               }
             },
+            {
+              type: "graph",
+              data: {
+                text:
+                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
+              }
+            },
           ],
           version: "2.12.4"
         }}
